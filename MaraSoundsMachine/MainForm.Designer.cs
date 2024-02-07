e821c910c2ecf1f1de65e5e69896ec904f4bcac8
WJC <kontractor.productions@gmail.com>
2024-02-06 18:35:29 -0800

    Fun stuff!

--- MaraSoundsMachine/MainForm.Designer.cs
+++ MaraSoundsMachine/MainForm.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.soundPanel_tabControl = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // soundPanel_tabControl
+            // 
+            this.soundPanel_tabControl.Location = new System.Drawing.Point(14, 13);
+            this.soundPanel_tabControl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.soundPanel_tabControl.Multiline = true;
+            this.soundPanel_tabControl.Name = "soundPanel_tabControl";
+            this.soundPanel_tabControl.SelectedIndex = 0;
+            this.soundPanel_tabControl.Size = new System.Drawing.Size(905, 459);
+            this.soundPanel_tabControl.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::MaraSoundsMachine.Properties.Resources.m1_sounds;
+            this.ClientSize = new System.Drawing.Size(933, 461);
+            this.Controls.Add(this.soundPanel_tabControl);
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "MainForm";
+            this.Text = "Marathon Sounds Machine v0.1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResizeEnd += new System.EventHandler(this.MainFormResized);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl soundPanel_tabControl;
     }
 }
 
--- MaraSoundsMachine/MainForm.cs
+++ MaraSoundsMachine/MainForm.cs
@@ -16,5 +16,17 @@ namespace MaraSoundsMachine
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainFormResized(object sender, EventArgs e)
+        {
+
+            // Adjust the size of the tab box now proportionally so
+            soundPanel_tabControl.Size = MainForm.Size;
+        }
     }
 }
