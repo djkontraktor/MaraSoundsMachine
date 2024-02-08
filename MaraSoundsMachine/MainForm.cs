39c2cf69467671d59447a54582294ce5a26d5523
WJC <kontractor.productions@gmail.com>
2024-02-07 18:52:03 -0800

    Scrap old ass DirectAudio code. Replace with shiny new XAudio2 NuGet package.

--- MaraSoundsMachine/MainForm.cs
+++ MaraSoundsMachine/MainForm.cs
@@ -7,9 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SharpDX;
-using SharpDX.DirectSound;
-using SharpDX.Multimedia;
+using SharpDX.XAudio2;
 
 namespace MaraSoundsMachine
 {
@@ -33,50 +31,7 @@ namespace MaraSoundsMachine
 
         private void PlayWave(Wave sample, bool looping)
         {
-            DirectSound directSound = new DirectSound();
-
-            // Set Cooperative Level to PRIORITY (priority level can call the SetFormat and Compact methods)
-            directSound.SetCooperativeLevel(this.Handle, CooperativeLevel.Priority);
             
-            WaveFormat waveFormat = new WaveFormat();
-
-            SoundBufferDescription primaryBufferDescription = new SoundBufferDescription();
-
-            primaryBufferDescription.BufferBytes = waveFormat.ConvertLatencyToByteSize(60000);
-
-            // Default WaveFormat Stereo 44100 kHz 16 bit
-            primaryBufferDescription.Format = waveFormat;
-
-            primaryBufferDescription.Flags = BufferFlags.GetCurrentPosition2 |
-                BufferFlags.ControlPositionNotify |
-                BufferFlags.GlobalFocus |
-                BufferFlags.ControlVolume |
-                BufferFlags.StickyFocus;
-
-            primaryBufferDescription.AlgorithmFor3D = Guid.Empty;
-
-            SecondarySoundBuffer primarySoundBuffer = new SecondarySoundBuffer(directSound, primaryBufferDescription);
-
-            BufferCapabilities capabilities = primarySoundBuffer.Capabilities;
-
-            DataStream dataPart2;
-
-            DataStream dataPart1 = primarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
-
-            int numberOfSamples = capabilities.BufferBytes / waveFormat.BlockAlign;
-
-            for (int i = 0; i < numberOfSamples; i++)
-            {
-                short value = (short)sample.WaveStream.ReadByte();
-                dataPart1.Write(value);
-                dataPart1.Write(value);
-            }
-
-            primarySoundBuffer.Unlock(dataPart1, dataPart2);
-
-            PlayFlags isLooping = (looping) ? PlayFlags.Looping : PlayFlags.None;
-
-            primarySoundBuffer.Play(0, isLooping);
         }
 
         private void ResizeTabControl()
